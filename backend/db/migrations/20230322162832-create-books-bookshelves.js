7d26b65f1de32d593c93ed42ce5ba6b0ebd00382
Zaineb Marediya <zmarediya@yahoo.com>
2023-03-22 18:11:58 -0500

    updated migration file

--- backend/db/migrations/20230322162832-create-books-bookshelves.js
+++ backend/db/migrations/20230322162832-create-books-bookshelves.js
@@ -8,12 +8,6 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Books_Bookshelves', {
-      // id: {
-      //   allowNull: false,
-      //   autoIncrement: true,
-      //   primaryKey: true,
-      //   type: Sequelize.INTEGER
-      // },
       bookId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -26,16 +20,6 @@ module.exports = {
         references: { model: "Bookshelves" },
         onDelete: "CASCADE"
       }
-      // createdAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE,
-      //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      // },
-      // updatedAt: {
-      //   allowNull: false,
-      //   type: Sequelize.DATE,
-      //   defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      // }
     }, options);
   },
   async down(queryInterface, Sequelize) {
