171efd913e5de6af39e344db4553efd3deb4ced6
Zaineb Marediya <zmarediya@yahoo.com>
2023-03-24 10:55:56 -0500

    updated session reducer to reflect more accurate user information

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,15 +38,13 @@ export const restoreUser = () => async dispatch => {
 };
 
 export const signup = (user) => async (dispatch) => {
-    const { username, firstName, lastName, email, password } = user;
+    const { name, email, password } = user;
     const response = await csrfFetch("/api/users", {
         method: "POST",
         body: JSON.stringify({
-            username,
-            firstName,
-            lastName,
+            name,
             email,
-            password,
+            password
         }),
     });
     const data = await response.json();
