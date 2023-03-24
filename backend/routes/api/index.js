d5d481a30edb202da0d6b8a0e7dcd5f1d4c326cd
Zaineb Marediya <zmarediya@yahoo.com>
2023-03-24 11:02:02 -0500

    removed unneeded files

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,11 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const booksRouter = require('./books.js');
 const bookshelvesRouter = require('./bookshelves.js')
-// const spotsRouter = require('./spots.js');
 // const reviewsRouter = require('./reviews.js');
-// const bookingsRouter = require('./bookings.js');
-// const spotImagesRouter = require('./spot-images.js')
-// const reviewImagesRouter = require('./review-images.js')
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
@@ -20,11 +16,7 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/books', booksRouter);
 router.use('/bookshelves', bookshelvesRouter);
-// router.use('/spots', spotsRouter);
 // router.use('/reviews', reviewsRouter);
-// router.use('/bookings', bookingsRouter);
-// router.use('/spot-images', spotImagesRouter);
-// router.use('/review-images', reviewImagesRouter);
 
 // GET /api/restore-user
 router.get(
