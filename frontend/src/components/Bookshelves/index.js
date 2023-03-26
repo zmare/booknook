33aab0016dd65653b7ac1048032205a02e7d734b
Zaineb Marediya <zmarediya@yahoo.com>
2023-03-26 18:40:23 -0500

    css

--- frontend/src/components/Bookshelves/index.js
+++ frontend/src/components/Bookshelves/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+//import { Link, NavLink } from 'react-router-dom';
+//import { useSelector } from 'react-redux';
+import './Bookshelves.css'
 
 const Bookshelves = () => {
 
+
+
+
     return (
-        <h1>Hello from bookshelf component</h1>
+        <>
+            <div className="bookshelf-nav-container">
+                <div className='bookshelf-nav-title'>
+                    My Books
+                </div>
+                <div className='bookshelf-settings'>
+                    <ul className='bookshelf-settings-ul'>
+                        <li> Batch Edit </li>
+                        <li> Settings </li>
+                        <li> Stats </li>
+                        <li> Print </li>
+                        {/* <li> | </li> */}
+                    </ul>
+                </div>
+            </div>
+            <div className="bookshelf-details-container">
+                <div className='bookshelf-sidebar-container'>
+                    <div className='bookshelves-header'>
+                        Bookshelves
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
+                <div className='bookshelf-book-details-container'>
+                    <div className='book-nav'>
+                        books nav
+                    </div>
+                    <div className='book-detail-index'>
+                        book detail
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
