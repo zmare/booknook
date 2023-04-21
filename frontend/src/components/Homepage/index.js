5aafbef37a85d852ba8aeee640ebc7662ac6d547
Zaineb Marediya <zmarediya@yahoo.com>
2023-04-20 22:23:06 -0500

    styling splashpage

--- frontend/src/components/Homepage/index.js
+++ frontend/src/components/Homepage/index.js
@@ -54,79 +54,174 @@ function Homepage() {
 
     return (
         <>
+            <div className='homepage-parent-container'>
+                <div className='banner-background-image'> </div>
+                <div className="left-right-container">
+                    <div className="homepage-left-container">
+                        <div className="left-para-container">
+                            <h2>Deciding what to read next?</h2>
+                            <p>
+                                You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.
+                                What are your friends reading?
 
-            <>
-                <div>
-                    <div className='banner-background-image'> </div>
-                    <div className="login-form-container">
-                        <div className="login-form">
-                            <form onSubmit={handleSubmit}>
-                                <h1 style={{ fontWeight: 'bolder' }} className='login-form-header'>Welcome to BookNook!</h1>
-                                <h2 className='login-form-header' id='small-header'>Log in to get started!</h2>
-                                <ul style={{ color: 'red', fontFamily: "'Roboto',sans-serif", fontSize: '12px', listStyle: 'none', paddingRight: '30px' }}>
-                                    {errors.map((error, idx) => (
-                                        <li key={idx}>{error}</li>
-                                    ))}
-                                </ul>
-                                <div>
-                                    <label className='login-form-label'>
-                                        Email
-                                        <br></br>
-                                        <input
-                                            className='login-form-input'
-                                            type="text"
-                                            value={credential}
-                                            onChange={(e) => setCredential(e.target.value)}
-                                            required
-                                        />
-                                    </label>
-                                </div>
-                                <br></br>
-                                <div>
-                                    <label className='login-form-label'>
-                                        Password
-                                        <br></br>
-                                        <input
-                                            className='login-form-input'
-                                            type="password"
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                            required
-                                        />
-                                    </label>
-                                </div>
-                                <br></br>
-                                <button className='login-form-button' type="submit">Log In</button>
-                                <button onClick={handleDemoLogin} type="text" className='login-form-button' id='demo-user-button'>Log In Demo User</button>
-                                <p className="login-form-footer">Not a member?<span> </span>
-                                    <NavLink to="/signup">Sign up!</NavLink>
-                                </p>
-                            </form>
+                                Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.
+                            </p>
+                        </div>
+                        <div className='right-para-container'>
+                            <h2>What are your friends reading?</h2>
+                            <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads. </p>
                         </div>
                     </div>
                 </div>
-                <div className="homepage-left-container">
-                    <div className="left-para-container">
-                        <h2>Deciding what to read next?</h2>
-                        <p>
-                            You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.
-                            What are your friends reading?
+                <div className='list-card-wrapper'>
+                    <div className='list-card'>
+                        <div id='list-card-1'>
+                            <p className="list-card-header">What will <span style={{ fontStyle: 'italic' }}>you</span> discover?</p>
+                            <div className='list-card-para'>
+                                <p>Because Deborah liked...</p>
+                                <p>She discovered:</p>
+                            </div>
+                            <div className='list-card-img-container'>
+                                <div >
+                                    <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                    <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                    <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
 
-                            Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.
-                        </p>
-                    </div>
-                    <div className='right-para-container'>
-                        <h2>What are your friends reading?</h2>
-                        <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads. </p>
-                    </div>
+                                </div>
 
+                                <div>
+                                    <img className='list-card-img' id='arrow' src="https://s.gr-assets.com/assets/home/discovery_arrow-f1e8677f2c8b68500ed82ef0d5b7c59b.png"></img>
+                                </div>
+
+                                <div>
+                                    <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div id='list-card-1'>
+                            <p>What will <span style={{ fontStyle: 'italic' }}>you</span> discover?</p>
+                            <div className='list-card-para'>
+                                <p>Because Deborah liked...</p>
+                                <p>She discovered:</p>
+                            </div>
+                            <div>
+                                <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                <img className='list-card-img' src="https://www.lse.ac.uk/International-History/Images/Books/NoBookCover.png"></img>
+                                <img className='list-card-img' id='arrow' src="https://s.gr-assets.com/assets/home/discovery_arrow-f1e8677f2c8b68500ed82ef0d5b7c59b.png"></img>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </>
-            {/* :
-                <HomepageLoggedIn /> */}
+
+
+            </div>
+
+
+            <div className="login-form-container">
+                <div className="login-form">
+                    <form onSubmit={handleSubmit}>
+                        <h1 style={{ fontWeight: 'bolder' }} className='login-form-header'>Welcome to BookNook!</h1>
+                        <h2 className='login-form-header' id='small-header'>Log in to get started!</h2>
+                        <ul style={{ color: 'red', fontFamily: "'Roboto',sans-serif", fontSize: '12px', listStyle: 'none', paddingRight: '30px' }}>
+                            {errors.map((error, idx) => (
+                                <li key={idx}>{error}</li>
+                            ))}
+                        </ul>
+                        <div>
+                            <label className='login-form-label'>
+                                Email
+                                <br></br>
+                                <input
+                                    className='login-form-input'
+                                    type="text"
+                                    value={credential}
+                                    onChange={(e) => setCredential(e.target.value)}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <br></br>
+                        <div>
+                            <label className='login-form-label'>
+                                Password
+                                <br></br>
+                                <input
+                                    className='login-form-input'
+                                    type="password"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </label>
+                        </div>
+                        <br></br>
+                        <button className='login-form-button' type="submit">Log In</button>
+                        <button onClick={handleDemoLogin} type="text" className='login-form-button' id='demo-user-button'>Log In Demo User</button>
+                        <p className="login-form-footer">Not a member?<span> </span>
+                            <NavLink to="/signup">Sign up!</NavLink>
+                        </p>
+                    </form>
+                </div>
+            </div>
 
         </>
     );
 }
 
 export default Homepage;
+
+
+/*  
+
+<div className="login-form-container">
+<div className="login-form">
+<form onSubmit={handleSubmit}>
+    <h1 style={{ fontWeight: 'bolder' }} className='login-form-header'>Welcome to BookNook!</h1>
+    <h2 className='login-form-header' id='small-header'>Log in to get started!</h2>
+    <ul style={{ color: 'red', fontFamily: "'Roboto',sans-serif", fontSize: '12px', listStyle: 'none', paddingRight: '30px' }}>
+        {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+        ))}
+    </ul>
+    <div>
+        <label className='login-form-label'>
+            Email
+            <br></br>
+            <input
+                className='login-form-input'
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+            />
+        </label>
+    </div>
+    <br></br>
+    <div>
+        <label className='login-form-label'>
+            Password
+            <br></br>
+            <input
+                className='login-form-input'
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+        </label>
+    </div>
+    <br></br>
+    <button className='login-form-button' type="submit">Log In</button>
+    <button onClick={handleDemoLogin} type="text" className='login-form-button' id='demo-user-button'>Log In Demo User</button>
+    <p className="login-form-footer">Not a member?<span> </span>
+        <NavLink to="/signup">Sign up!</NavLink>
+    </p>
+</form>
+</div>
+</div>
+
+*/
